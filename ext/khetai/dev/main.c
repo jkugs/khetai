ac5cee8adef2145b2913748859c05f4a2747a597
jkugs <jkuglics@gmail.com>
2022-03-05 13:21:18 -0500

    formatting

--- ext/khetai/dev/main.c
+++ ext/khetai/dev/main.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include "../khetai_lib.h"
 
-char* init_board[120] =
+char *init_board[120] =
     {"--", "--", "--", "--", "--", "--", "--", "--", "--", "--", "--", "--",
      "--", "L2", "--", "--", "--", "A2", "X2", "A2", "P1", "--", "--", "--",
      "--", "--", "--", "P2", "--", "--", "--", "--", "--", "--", "--", "--",
@@ -17,33 +17,34 @@ char* init_board[120] =
 
 time_t start_time;
 int max_time;
-     
-int main() {
+
+int main()
+{
     init_zobrist();
-    srand((unsigned) time(NULL));
-    
+    srand((unsigned)time(NULL));
+
     setup_board(init_board);
     print_board();
-    
+
     start_time = time(NULL);
     max_time = 5;
     int max_depth = 25;
-    
+
     int depth = 1;
     Move best_move = (Move)0;
     Move current_move = (Move)0;
-    while((time(NULL) - start_time < max_time) && (depth <= max_depth))
+    while ((time(NULL) - start_time < max_time) && (depth <= max_depth))
     {
         best_move = current_move;
         current_move = alphabeta_root(depth, Red);
         depth++;
     }
     make_move(best_move);
-    
+
     printf("\n========================\n\n");
     print_board();
-    
-    printf("\nDEPTH: %d\n", depth-1);
-    
+
+    printf("\nDEPTH: %d\n", depth - 1);
+
     return 0;
 }
